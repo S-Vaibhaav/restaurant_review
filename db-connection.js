2ccf738dc6e68c20f1fb28dcf6de660d3f7c629c
S-Vaibhaav <66787237+S-Vaibhaav@users.noreply.github.com>
2022-08-16 16:16:45 +0800

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -1,58 +1,15 @@
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//     host:'localhost',
-//     port: '3306',
-//     user:'root',
-//     password:'cdev',
-//     database: 'restaurant_review'
-// });
+ var mysql = require('mysql');
+ var connection = mysql.createConnection({
+     host:'localhost',
+     port: '3306',
+     user:'root',
+     password:'cdev',
+     database: 'restaurant_review'
+ });
 
-// connection.connect(err => {  // test out connetion and console.log error if there is one
-//     if (err) throw err;
-//     console.log('Connected To DB');
-// }); 
-// module.exports = connection;
-var mysql = require('mysql');
-
-var connection = mysql.createConnection({
-
-    host:'dbrestaurant.cefojpvupkf7.us-east-1.rds.amazonaws.com',
-
-    port: '3306',
-
-    user:'rest',
-
-    password:'tG068098',
-
-    database: 'restaurant_review'
-
-});
-
-
-connection.connect(err => {  // test out connetion and console.log error if there is one
-
-    if (err) throw err;
-
-    console.log('Connected To DB');
-
-});
-
-
-
-var express = require('express');
-var app = express();
-app.route('/mysql').get(fromMYSQL);
-
-
-function fromMYSQL(request, respond){
-
-    var sql = "Select * from restaurant";
-connection.query(sql, function (err,result){
-    console.log(result);
-})
-
-}
-
-app.listen(3000);
-console.log("http://127.0.0.1:3000/mysql%22");
+ connection.connect(err => {  // test out connetion and console.log error if there is one
+     if (err) throw err;
+     console.log('Connected To DB');
+ }); 
+ module.exports = connection;
 
