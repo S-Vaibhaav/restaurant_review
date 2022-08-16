a24e103383bc00f29c99a942bf7742d5e2a1ee62
S-Vaibhaav <66787237+S-Vaibhaav@users.noreply.github.com>
2022-08-16 16:35:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ app.route('/favourites').post(favouriteController.addToFavourites)
 
 
 
-app.listen(8080, "127.0.0.1");     // start the nodejs to be listening for incoming request @ port 8080
-console.log("web server running @ http://127.0.0.1:8080"); // output to console
+//app.listen(8080, "127.0.0.1");     // start the nodejs to be listening for incoming request @ port 8080
+console.log("web server running @ http://ec2-54-234-35-39.compute-1.amazonaws.com:8080"); // output to console
